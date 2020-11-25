910f02a3a79e29c3c9643a052fb1882fa07f4b9b
Roshan nahak <roshannahak1122@gmail.com>
2020-11-26 03:59:35 +0530

    complete login and registration ui

--- app/src/main/java/com/android/tusk/Login_screen.java
+++ app/src/main/java/com/android/tusk/Login_screen.java
@@ -1,18 +1,43 @@
 package com.android.tusk;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.res.ResourcesCompat;
 
-import android.graphics.Typeface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 public class Login_screen extends AppCompatActivity {
+
+    RelativeLayout submitbtn;
+    TextView signupbtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_screen);
 
+        initializedViews();
+
+        signupbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Login_screen.this, Register_screen.class));
+            }
+        });
+
+        submitbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Login_screen.this, Dashboard.class));
+            }
+        });
+
+    }
+
+    private void initializedViews() {
+        submitbtn = findViewById(R.id.submit_button);
+        signupbtn = findViewById(R.id.signup_textview);
     }
 }
---
+++ app/src/main/java/com/android/tusk/Register_screen.java
@@ -0,0 +1,16 @@
+package com.android.tusk;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class Register_screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register_screen);
+
+    }
+}
