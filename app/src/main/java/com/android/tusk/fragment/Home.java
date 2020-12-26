539cab9f432a7ae405a03012e2293e4424c9631a
Roshan nahak <roshannahak1122@gmail.com>
2020-12-26 14:05:30 +0530

    update home screen added today tasks

--- app/src/main/java/com/android/tusk/fragment/Home.java
+++ app/src/main/java/com/android/tusk/fragment/Home.java
@@ -3,15 +3,21 @@ package com.android.tusk.fragment;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.core.widget.NestedScrollView;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
+import com.android.tusk.Dashboard;
 import com.android.tusk.R;
 import com.android.tusk.adapter.NewTaskAdapter;
 import com.android.tusk.detailed_view;
@@ -20,8 +26,13 @@ import com.android.tusk.model.Milestone;
 import com.android.tusk.model.Task;
 import com.android.tusk.retrofit.APIclient;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -39,6 +50,10 @@ public class Home extends Fragment implements NewTaskAdapter.taskDetailedView {
 
     RecyclerView newTaskRecycler;
     List<Task> taskArrayList;
+
+    LinearLayout taskabilitymsg, connectionerrormsg;
+    ProgressBar content_progress;
+    NestedScrollView content;
 
     public Home() {
         // Required empty public constructor
@@ -62,29 +77,65 @@ public class Home extends Fragment implements NewTaskAdapter.taskDetailedView {
     }
 
     private void fetchNewTaskdate() {
+        content_progress.setVisibility(View.VISIBLE);
         Call<AllTask> allTaskCall = APIclient.getInterface().getAllTask();
         allTaskCall.enqueue(new Callback<AllTask>() {
             @Override
             public void onResponse(Call<AllTask> call, Response<AllTask> response) {
-                if (response.isSuccessful()){
+                if (response.isSuccessful()) {
                     AllTask allTask = response.body();
                     taskArrayList = new ArrayList<>();
 
-                    for (Task task: allTask.getTasks()){
-                        taskArrayList.add(task);
-                    }
+                    Date date = Calendar.getInstance().getTime();
+                    String currentdate = String.valueOf(date);
 
-                    NewTaskAdapter taskAdapter = new NewTaskAdapter(getContext(), taskArrayList);
-                    taskAdapter.setOnItemViewClickListener(Home.this);
-                    newTaskRecycler.setAdapter(taskAdapter);
+                    for (Task task : allTask.getTasks()) {
+                        if (dateFormate(task.getCreatedAt(), 1).equals(dateFormate(currentdate, 2))) {
+                            taskArrayList.add(task);
+                        }
+                    }
+                    if (!taskArrayList.isEmpty()) {
+                        newTaskRecycler.setVisibility(View.VISIBLE);
+                        taskabilitymsg.setVisibility(View.GONE);
+                        NewTaskAdapter taskAdapter = new NewTaskAdapter(getContext(), taskArrayList);
+                        taskAdapter.setOnItemViewClickListener(Home.this);
+                        newTaskRecycler.setAdapter(taskAdapter);
+                    } else {
+                        newTaskRecycler.setVisibility(View.GONE);
+                        taskabilitymsg.setVisibility(View.VISIBLE);
+                    }
+                    content_progress.setVisibility(View.GONE);
+                    content.setVisibility(View.VISIBLE);
                 }
             }
 
             @Override
             public void onFailure(Call<AllTask> call, Throwable t) {
                 ToastMassage("failed");
+                content_progress.setVisibility(View.GONE);
+                connectionerrormsg.setVisibility(View.VISIBLE);
             }
         });
+    }
+
+    private String dateFormate(String rawdate, int i) {
+        Date date = null;
+        String format;
+        try {
+            if (i == 1) {
+                date = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", Locale.getDefault()).parse(rawdate);
+                format = new SimpleDateFormat("dd/MM/yyyy", Locale.getDefault()).format(date);
+                return format;
+            }
+            if (i == 2) {
+                date = new SimpleDateFormat("EEE MMM dd HH:mm:ss zzz yyyy", Locale.getDefault()).parse(rawdate);
+                format = new SimpleDateFormat("dd/MM/yyyy", Locale.getDefault()).format(date);
+                return format;
+            }
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return rawdate;
     }
 
     private void ToastMassage(String msg) {
@@ -95,6 +146,11 @@ public class Home extends Fragment implements NewTaskAdapter.taskDetailedView {
         newTaskRecycler = view.findViewById(R.id.fragment_newTask_recyclerView);
         newTaskRecycler.setLayoutManager(new LinearLayoutManager(getContext()));
         newTaskRecycler.setHasFixedSize(true);
+
+        taskabilitymsg = view.findViewById(R.id.task_ability_massage);
+        content_progress = view.findViewById(R.id.circulre_progress);
+        content = view.findViewById(R.id.home_content);
+        connectionerrormsg = view.findViewById(R.id.connection_error_msg);
     }
 
     @Override
@@ -104,7 +160,7 @@ public class Home extends Fragment implements NewTaskAdapter.taskDetailedView {
 
         //init model
         Milestone milestones = new Milestone();
-        for (Milestone milestone : task.getMilestones()){
+        for (Milestone milestone : task.getMilestones()) {
             milestoneList.add(milestone);
         }
 
